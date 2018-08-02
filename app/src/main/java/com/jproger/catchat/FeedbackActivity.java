57e688e8ed1ff78a5878be4f73a454fafdf0366e
Jproger <webscripting@ya.ru>
2018-08-02 16:47:21 +0300

    Add fragments and acts

--- app/src/main/java/com/jproger/catchat/FeedbackActivity.java
+++ app/src/main/java/com/jproger/catchat/FeedbackActivity.java
@@ -4,12 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
-public class HelpActivity extends AppCompatActivity {
+public class FeedbackActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_help);
+        setContentView(R.layout.activity_feedback);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
